2d7ee13eddc5bc9f7192d507cb2b6068ebe6bf9e
lizaetillman <lizaetillman@gmail.com>
2021-02-19 02:50:57 -0500

    modify sessions C and V

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to dashboard_path
+        else
+            #add flash error message "Invalid Credentials Given. Please try again."
+            redirect_to 'login'
+        end
     end
 
     def home
